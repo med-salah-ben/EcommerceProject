18826252a8e2572bb8f05340d053499862831fe7
med-salah-ben <med.salah.ben.amou@gmail.com>
2020-12-01 00:54:53 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import Signup from "./Components/Signup";
+import Signin from "./Components/Signin";
+import Home from "./Components/Home";
+import AddProduct from "./Components/AddProduct";
+import HomeScreen from "./Components/HomeScreen";
+import AddAdmin from "./Components/AddAdmin";
+import Cart from "./Components/Cart";
+import Header from "./Components/Header";
+import OrderAdmin from "./Components/OrderAdmin";
+import ConfirmProduct from "./Components/ConfirmProduct";
+import AllUsers from "./Components/AllUsers";
+import Footer from "./Components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="grid-container">
+          <Header />
+
+          <Route exact path="/allusers" render={() => <AllUsers />} />
+          <Route exact path="/register" render={() => <Signup />} />
+          <Route exact path="/login" render={() => <Signin />} />
+          <Route exact path="/" render={() => <Home />} />
+          <Route exact path="/addproduct" render={() => <AddProduct />} />
+          <Route exact path="/OurProduct" render={() => <HomeScreen />} />
+          <Route exact path="/addadmin" render={() => <AddAdmin />} />
+          <Route exact path="/cart" render={() => <Cart />} />
+          <Route exact path="/adminOrders" render={() => <OrderAdmin />} />
+          <Route
+            exact
+            path="/confirmProduct"
+            render={() => <ConfirmProduct />}
+          />
+          <Footer />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
